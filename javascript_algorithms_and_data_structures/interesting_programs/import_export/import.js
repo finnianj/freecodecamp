3ae0358afb28d8d6b0e9bc904ad5ce7db09bc48b
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2022-12-18 14:54:38 +0100

    k

--- javascript_algorithms_and_data_structures/interesting_programs/import_export/import.js
+++ javascript_algorithms_and_data_structures/interesting_programs/import_export/import.js
@@ -4,3 +4,7 @@ import { subtract, multiply, divide } from "./another_export.js"
 console.log(variable(2, 4));
 console.log(subtract(2, 4));
 console.log(multiply(2, 4));
+
+//package.json file is essential as otherwise the import and export files are not able to
+//communicate with one another. Furthermore, in package.json you need to set the type to module.
+//package.json is like the commander of all your javascript files.
