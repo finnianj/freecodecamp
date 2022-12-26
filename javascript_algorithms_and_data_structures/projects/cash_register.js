c6c08390eb7d0491944c0b564d9cc75ffa95f368
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2022-12-26 12:38:14 +0000

    updatd solution

--- javascript_algorithms_and_data_structures/projects/cash_register.js
+++ javascript_algorithms_and_data_structures/projects/cash_register.js
@@ -1,43 +1,31 @@
 function checkCashRegister(price, cash, cid) {
-  let changeToGive = (cash - price) * 100;
-  let denoms = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1]
-  let changeGiven = [
-    ["ONE HUNDRED", 0],
-    ["TWENTY", 0],
-    ["TEN", 0],
-    ["FIVE", 0],
-    ["ONE", 0],
-    ["QUARTER", 0],
-    ["DIME", 0],
-    ["NICKEL", 0],
-    ["PENNY", 0],
-  ];
+  let changeRequired = cash - price + 0.001;
+  let returnChange = cid.reverse().map(item => [item[0], 0]);
+  let cashRegister = cid.map(item => [item[0], item[1]])
+  let registerSum = cashRegister.reduce((a, b) => a + b[1], 0);
+  let amounts = [100, 20, 10, 5, 1, 0.25, 0.1, 0.05, 0.01]
 
-  let register = [...cid].reverse().map(item => [item[0], item[1] * 100]);
-  let sum = register.reduce((a, b) => (a + b[1]),0) / 100
-  if (sum == changeToGive / 100) {
-    return {status: "CLOSED", change: [...cid]}
-  } else {
-    for (let i = 0; i < register.length; i++) {
-      while (denoms[i] <= changeToGive && register[i][1] > 0) {
-        changeGiven[i][1] += denoms[i];
-        changeToGive -= denoms[i];
-        register[i][1] -= denoms[i];
-      }
+  if (registerSum == changeRequired - 0.001) {
+    return {status: "CLOSED", change: cashRegister.reverse().map(item => [item[0], item[1]])}
+  }
+
+  for (let i = 0; i < cashRegister.length; i++) {
+    while(amounts[i] <= changeRequired && cashRegister[i][1] > 0) {
+      cashRegister[i][1] -= amounts[i];
+      changeRequired -= amounts[i];
+      returnChange[i][1] += amounts[i];
     }
   }
 
+  returnChange = returnChange.filter(item => item[1] != 0)
+  let returnSum = returnChange.reduce((a, b) => a + b[1], 0)
 
-let change = changeGiven.map(item => [item[0], item[1] / 100]).filter(coin => coin[1] != 0)
+  if (returnSum < changeRequired) {
+    return {status: "INSUFFICIENT_FUNDS", change: []}
+  }
 
-let total = change.reduce((a, b) => a + b[1], 0)
+  return {status: "OPEN", change: returnChange}
 
-if (total < changeToGive) {
-  return {status: "INSUFFICIENT_FUNDS", change: []};
-}
+  }
 
-console.log(change)
-return {status: "OPEN", change: change}
-}
-
-checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
+  checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])
