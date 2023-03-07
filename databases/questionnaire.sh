703678477e1accb06f3da959b8b7475d21b6a2be
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-07 15:49:10 +0100

    learning basic bash scripting, chmod acces

---
+++ databases/questionnaire.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+
+echo ~~ Questionnaire ~~
+echo $QUESTION1
+read NAME
+echo $QUESTION2
+read LOCATION
+echo Hello $NAME from $LOCATION.
