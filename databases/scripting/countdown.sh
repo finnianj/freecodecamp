61773adda48a4c5278e571f5cf730f28a314d23e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-07 16:21:52 +0100

    using test -a flag

--- databases/scripting/countdown.sh
+++ databases/scripting/countdown.sh
@@ -14,8 +14,14 @@ else
 fi
 #using conditional statement
 #You can compare integers inside the brackets ([[ ... ]]) of your if with -eq (equal), -ne (not equal), -lt (less than), -le (less than or equal), -gt (greater than), -ge (greater than or equal).
+
 #Each command has an exit status that can be accessed with $?. View the exit status of the last command with echo $?
-#0 is true, 1 is false
+#0 is true, 1 is false. It counts the number of errors
 
 [[ 4 -ge 5 ]]; echo $?
 #you can run separate commands on a single line with ;. $? prints the exit status of the last command.
+
+
+#run help test to see all the different parameters you can use to evaluate expression
+[[ -a countdown.sh ]]; echo $?
+#this will have an exit status of 0, because the -a flag checks if a file exists
