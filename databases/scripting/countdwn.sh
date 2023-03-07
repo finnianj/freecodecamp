70400c87ef92dc5f2e939708e1e5480399a7796d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-07 16:04:53 +0100

    bash conditionals

--- databases/scripting/countdwn.sh
+++ databases/scripting/countdwn.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 #Program that counts down to zero from a given argument
+
 echo $*
 #prints all given arguments. arguments are given in terminal like so: ./countdown.sh arg1 arg2 arg3
+echo $1
+#prints first argument
+
+if [[ $1 == arg1 ]]
+then
+  echo true
+fi
+#using conditional statement
