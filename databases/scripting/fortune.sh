46dd52d9bc387a323a3ba72be0f873c61dc1cc57
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-08 13:52:59 +0100

    fortune teller

--- databases/scripting/fortune.sh
+++ databases/scripting/fortune.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Program to tell a persons fortune
 
+#-------NOTES--------------
+
+# defining an array: ARR=("a" "b" "c")
+# printing item 2: echo ${ARR[1]}
+# printing entire array: echo ${ARR[*]}
+
+#--------------------------
+
 echo -e "\n~~ Fortune Teller ~~\n"
