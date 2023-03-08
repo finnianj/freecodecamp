a24e26a80ac386450423f867330ac0cd70f3ab94
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-08 13:24:07 +0100

    bingo

--- databases/scripting/bingo.sh
+++ databases/scripting/bingo.sh
@@ -14,3 +14,8 @@ echo $NUMBER
 
 #because BASH sees everything as a string, here NUMBER will be NUMBER%75.
 # doubles parentheses with a $ will provide the result of a calculation as output. If you don't put $ before (()), then the calculation will be performed but not have output. It is good for changing variable values.
+# So, if you want to do something with the result of a calculation, use $(()).
+
+declare -p NUMBER
+
+#you can view variables that you have stored with declare -p variable_name.
