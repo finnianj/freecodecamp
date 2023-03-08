40afe3c4db546dcd64b6179f92fb6f1329fa9803
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-08 13:12:04 +0100

    bingo

--- databases/scripting/bingo.sh
+++ databases/scripting/bingo.sh
@@ -1,9 +1,15 @@
-#!bin/bash
+#!/bin/bash
 # Bingo Number Generator
 
 echo -e "\n~~ Bingo Number Generator ~~\n"
-NUMBER=5
-echo $NUMBER
 
 #printenv will print all environment variables to the terminal. Every shell comes with preloaded environment variables.
 #declare -p will print all the variables in the shell. One of these is RANDOM, which gives a randome number
+
+NUMBER=$RANDOM%75
+
+#modulo defines the range [0-75]
+
+echo $NUMBER
+
+#because BASH sees everything as a string, here NUMBER will be NUMBER%75.
