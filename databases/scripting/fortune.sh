9b6244ba0dba300994c4c180df4f2fce3cd57e51
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-08 14:08:48 +0100

    fortune teller

--- databases/scripting/fortune.sh
+++ databases/scripting/fortune.sh
@@ -7,6 +7,8 @@
 # printing item 2: echo ${ARR[1]}
 # printing entire array: echo ${ARR[*]}
 
+# testing for pattern matching with =~. Also accepts REGEXP
+
 #--------------------------
 
 echo -e "\n~~ Fortune Teller ~~\n"
@@ -14,5 +16,15 @@ echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
 
 N=$(( RANDOM % 6 ))
+
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+  read QUESTION
+}
+
+until [[ $QUESTION =~ \?$ ]]
+do
+  GET_FORTUNE
+done
 
 echo ${RESPONSES[$N]}
