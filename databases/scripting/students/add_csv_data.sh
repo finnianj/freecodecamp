93c5b9e6512e6dd21d7979a5e1ba493b41498a7a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-09 16:10:28 +0100

    looping

--- databases/scripting/students/add_csv_data.sh
+++ databases/scripting/students/add_csv_data.sh
@@ -6,3 +6,12 @@ do
 done
 
 # reads each line of the csv and saves it into two variables, major and course
+
+# There's a default IFS variable in bash. IFS stands for "Internal Field Separator". View it with declare -p IFS
+# This variable is used to determine word boundaries. It defaults to spaces, tabs, and new lines. This is why the MAJOR variable was set to only the first word on each line from the data. Between the while and read commands, set the IFS to a comma like this: IFS=","
+# Now, it should use the comma in the data to separate words instead of spaces. Run the script again to see if it's working.
+
+cat courses.csv | while IFS="," read MAJOR COURSE
+do
+  echo $MAJOR
+done
