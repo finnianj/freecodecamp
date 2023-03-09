bacd18bc465634d06be16c7efd0cb2dd99371d7e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-09 16:25:07 +0100

    writing sql queries inside bash script

--- databases/scripting/students/add_csv_data.sh
+++ databases/scripting/students/add_csv_data.sh
@@ -34,4 +34,27 @@ done
 
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 
-# This variables will allow us to make queries
+# This variable will allow us to make queries:
+
+MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+
+
+cat courses.csv | while IFS="," read MAJOR COURSE
+do
+# get major_id
+  MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+  echo $MAJOR_ID
+# if not found
+  if [[ -z $MAJOR_ID ]]
+  then
+# insert major
+INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+echo $INSERT_MAJOR_RESULT
+# get new major_id
+  fi
+# get course_id
+# if not found
+# insert course
+# get new course_id
+# insert into majors_courses
+done
