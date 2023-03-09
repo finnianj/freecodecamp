fe906c2ec22d1c08d6bdf9b2611a3948f16d7c8e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-09 16:46:48 +0100

    truncation

--- databases/scripting/students/add_csv_data.sh
+++ databases/scripting/students/add_csv_data.sh
@@ -36,6 +36,10 @@
 
 # This variable will allow us to make queries:
 
+echo $($PSQL "TRUNCATE students, majors, courses, majors_courses")
+
+# deletes all rows in the above tables.
+
 cat courses.csv | while IFS="," read MAJOR COURSE
 do
   if [[ $MAJOR != major ]]
@@ -43,14 +47,46 @@ do
     # get major_id
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     # if not found
+
     if [[ -z $MAJOR_ID ]]
     then
       # insert major
       INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
+
       if [[ $INSERT_MAJOR_RESULT == 'INSERT 0 1' ]]
       then
         echo Inserted into majors, $MAJOR
       fi
+
       # get new major_id
+      MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
     fi
+
   fi
+
+  # get course_id
+  if [[ $COURSE != 'course' ]]
+    then
+    COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+
+    # if not found
+    if [[ -z $COURSE_ID ]]
+    then
+      # insert course
+      INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
+
+      if [[ $INSERT_COURSE_RESULT == 'INSERT 0 1' ]]
+      then
+        echo Inserted into courses, $COURSE
+      fi
+
+      # get new course_id
+      COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
+      # get new course_id
+    fi
+
+
+  fi
+
+# insert into majors_courses
+done
