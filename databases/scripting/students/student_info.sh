360ef6c1161280a2df336468fe1ea99be27ae58c
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-10 14:10:20 +0100

    querying db script

--- databases/scripting/students/student_info.sh
+++ databases/scripting/students/student_info.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Info about my computer science students from students database
- echo -e '\n~~ My Computer Science Students ~~\n'
+echo -e '\n~~ My Computer Science Students ~~\n'
+
+PSQL="psql -X --username=finncj --dbname=students --no-align --tuples-only -c"
+echo -e 'First name, last name, and GPA of students with a 4.0 GPA:'
+
+echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa >= 4.0")"
