1219764ec0ae6d2ba5d6d3c5395c671b2bdf476f
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-10 14:16:27 +0100

    queries

--- databases/scripting/students/student_info.sh
+++ databases/scripting/students/student_info.sh
@@ -3,6 +3,9 @@
 echo -e '\n~~ My Computer Science Students ~~\n'
 
 PSQL="psql -X --username=finncj --dbname=students --no-align --tuples-only -c"
-echo -e 'First name, last name, and GPA of students with a 4.0 GPA:'
 
+echo -e 'First name, last name, and GPA of students with a 4.0 GPA:'
 echo "$($PSQL "SELECT first_name, last_name, gpa FROM students WHERE gpa >= 4.0")"
+
+echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
+echo "$($PSQL "SELECT course FROM courses WHERE course < 'D'")"
