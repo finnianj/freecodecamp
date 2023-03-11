480fefc1f1c439520f4a8aa1fe61b8be5357036f
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-11 11:36:09 +0100

    inserting game data

--- databases/worldcup/insert_data.sh
+++ databases/worldcup/insert_data.sh
@@ -53,3 +53,23 @@ do
 
 
 done
+
+#-------game data-------
+
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+
+  if [[ $YEAR != year ]]
+  then
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    ADD_GAME=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+
+    if [[ $ADD_GAME == 'INSERT 0 1' ]]
+    then
+      echo Added $YEAR $ROUND between $WINNER and $OPPONENT
+    fi
+
+  fi
+
+done
