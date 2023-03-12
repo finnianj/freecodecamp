774332005a2f934fc4a975988cfa12cdf020f772
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-12 15:53:11 +0100

    grep

--- databases/scripting/advanced_bash/more_notes.sh
+++ databases/scripting/advanced_bash/more_notes.sh
@@ -1,3 +1,16 @@
 # You can print the word count using wc. Check out man wc for details
 
 wc notes.sh
+
+touch info.txt
+
+echo '#!/bin/bash' > info.txt
+echo '\n'Number of words: >> info.txt
+cat notes.sh | wc -w >> info.txt
+
+cat info.txt
+
+# You can use grep to find matching patterns in a file. Check out man grep fo options.
+# One cool one is --color, which highlights matching patterns.
+
+grep --color 'Finn' notes.sh
