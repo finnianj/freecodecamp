4ad2e582e41b8a25d63cf5ee6b2fa2d80c7a79af
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-12 15:04:43 +0100

    bash notes

--- databases/scripting/advanced_bash/notes.sh
+++ databases/scripting/advanced_bash/notes.sh
@@ -32,3 +32,16 @@ read NAME < file.txt
 # You can use the read command to assign the NAME variable to the contents of name.txt by redirecting the stdin
 
 echo $NAME
+
+# Another way to set the stdin is by using the pipe (|). It will use the output from one command as input for another.
+# Here's an example:
+
+echo Jeff | read $NAME
+echo $NAME
+
+# It worked, but not really. When you used the pipe (|) to set the input for read, it ran the command in a subshell or subprocess.
+# Basically, another terminal instance within the one you see.
+
+# cat also takes input
+
+echo Finn | cat
