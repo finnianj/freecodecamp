97ba1576c065dff5ef0777cc048a826927f2b1d3
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-12 16:04:34 +0100

    grep

--- databases/scripting/advanced_bash/more_notes.sh
+++ databases/scripting/advanced_bash/more_notes.sh
@@ -5,12 +5,18 @@ wc notes.sh
 touch info.txt
 
 echo '#!/bin/bash' > info.txt
-echo '\n'Number of words: >> info.txt
+echo '\n'Number of words in notes.sh: >> info.txt
 cat notes.sh | wc -w >> info.txt
 
 cat info.txt
 
 # You can use grep to find matching patterns in a file. Check out man grep fo options.
 # One cool one is --color, which highlights matching patterns.
+# Another is -n, which prints the line number
+# Another is -o, which prints every match on a new line.
 
-grep --color 'Finn' notes.sh
+grep --color -n 'Finn' notes.sh
+
+echo '\n'Number of times Finn appears in info.sh: >> info.txt
+
+grep -o 'Finn' notes.sh | wc -l >> info.txt
