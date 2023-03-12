1d0b4e046028934319258c73697966e508fc8341
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-12 14:52:57 +0100

    bash notes

--- databases/scripting/advanced_bash/notes.sh
+++ databases/scripting/advanced_bash/notes.sh
@@ -1,12 +1,23 @@
-There are two types of output, stdout and stderr.
-If a command is succesfull, the output is stdout.
-If a command throws an error, the output is stderr.
+# You can create or overwrite a file using >
 
-You can redirect the output using 1> for stdout and 2> for stderr.
+echo My name is Finn > file.txt
 
-For example:
+# If there is anything in the file, it would be overwritten.
+
+echo I am a drummer >> file.txt
+
+# The double arrow appends to the file, so the previous content is not overwritten.
+
+
+# There are two types of output, stdout and stderr.
+# If a command is succesfull, the output is stdout.
+# If a command throws an error, the output is stderr.
+
+# You can redirect the output using 1> for stdout and 2> for stderr.
+
+# For example:
 
 echo Hello there 1> file.txt
 bad_command 2> file.txt
 
-this creates a new file and send the output there. Nothing is printed in the terminal.
+# This creates a new file and send the output there. Nothing is printed in the terminal.
