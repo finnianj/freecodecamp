201b75934162dd85c04a0d273613e53fea5a2c41
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-13 12:09:26 +0100

    sed

--- databases/scripting/advanced_bash/more_notes.sh
+++ databases/scripting/advanced_bash/more_notes.sh
@@ -43,5 +43,5 @@ wc -l halo_2.txt >> halo_2_info.txt
 echo '\n'~~ Number of times the word Covenant appears:~~ >> halo_2_info.txt
 grep -c 'Covenant'halo_2.txt >> halo_2_info.txt
 
-echo '\n'~~ Number of times the word Covenant appears:~~ >> halo_2_info.txt
-
+echo '\n'~~ Lines that the word Covenant appears:~~ >> halo_2_info.txt
+grep -n 'Covenant' halo_2.txt | sed -E 's/([0-9]+).*/\1/' >> halo_2_info.txt
