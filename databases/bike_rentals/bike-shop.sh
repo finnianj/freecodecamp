fdb79d3afb0e0715a3879cc0f5de3f0c53076933
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-13 15:37:25 +0100

    rent menu update

--- databases/bike_rentals/bike-shop.sh
+++ databases/bike_rentals/bike-shop.sh
@@ -25,7 +25,6 @@ MAIN_MENU() {
 RENT_MENU() {
   # get available bikes
   AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available=true ORDER BY bike_id")
-  echo "$AVAILABLE_BIKES"
   # if no bikes available
   if [[ -z $AVAILABLE_BIKES ]]
   then
@@ -33,9 +32,22 @@ RENT_MENU() {
     MAIN_MENU "Sorry, we don't have any bikes available right now."
   else
   # display available bikes
+    echo -e "\nHere are the bikes we have available:"
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+    do
+      echo "$BIKE_ID) $SIZE\" $TYPE Bike"
+    done
+
   # ask for bike to rent
+  echo -e "\nWhich one would you like to rent?"
+  read BIKE_ID_TO_RENT
   # if input is not a number
-  # send to main menu
+  if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
+    then
+    # send to main menu
+    MAIN_MENU "That is not a valid bike number."
+  fi
+
   fi
 
 }
