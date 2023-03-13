fe97d857c96e6249c38f9cc6e287ad3319f1f7f4
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-13 15:13:52 +0100

    menu design

--- databases/bike_rentals/bike-shop.sh
+++ databases/bike_rentals/bike-shop.sh
@@ -4,6 +4,13 @@ echo -e '\n~~~~~ Bike Rental Shop ~~~~~\n'
 MAIN_MENU() {
   echo How may I help you?
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU ;;
+  esac
 }
 
 RENT_MENU() {
