3eb77b91c68cc3ce41c5368276dea5b1d79dd725
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-14 09:55:39 +0100

    insert into customers

--- databases/bike_rentals/bike-shop.sh
+++ databases/bike_rentals/bike-shop.sh
@@ -59,6 +59,20 @@ RENT_MENU() {
       then
       # send to main menu
         MAIN_MENU "That bike is not available."
+      else
+      # get customer info
+        echo -e "\nWhat's your phone number?"
+        read PHONE_NUMBER
+        CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$PHONE_NUMBER'")
+        # if customer doesn't exist
+        if [[ -z $CUSTOMER_NAME ]]
+        then
+          # get new customer name
+          echo -e "\nWhat's your name?"
+          read CUSTOMER_NAME
+          # insert new customer
+          INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$PHONE_NUMBER')")
+        fi
       fi
     fi
   fi
