0a09d3630c5add956494773f1a1efb7aa0e7775d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-16 15:38:28 +0100

    gettin started

--- databases/periodic_table/element.sh
+++ databases/periodic_table/element.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
+
 if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
 else
 
+  USER_INPUT=$1
+  if [[ $USER_INPUT =~ ^[0-9]+$ ]]
+  then
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE
+    atomic_number=$USER_INPUT")
+  else
+    ELEMENT=$($PSQL "SELECT * FROM elements WHERE
+    symbol LIKE '$USER_INPUT' OR
+    name LIKE '$USER_INPUT'")
+  fi
+  echo $ELEMENT
 fi
