a756a51f56c896ec5de2d51b32056bc5b929e02a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-17 15:58:23 +0100

    if user

--- databases/number_guess/number_guess.sh
+++ databases/number_guess/number_guess.sh
@@ -8,7 +8,19 @@ USERNAME="$(echo -e $INPUT | sed -r 's/^ *| *$//g')"
 
 USER=$($PSQL "SELECT * FROM users WHERE name LIKE '$USERNAME'")
 
-
+if [[ -z $USER ]]
+then
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(name) VALUES('$USERNAME')")
+  echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE name LIKE '$USERNAME'")
+  NUMBER_OF_GAMES=0
+else
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE name LIKE '$USERNAME'")
+  NUMBER_OF_GAMES=$($PSQL "SELECT COUNT(*) FROM games WHERE user_id=$USER_ID")
+  BEST_GAME_GUESSES=$($PSQL "SELECT guesses FROM games WHERE user_id=$USER_ID ORDER BY guesses LIMIT 1")
+  echo your best game: $BEST_GAME_GUESSES
+  echo -e "\nWelcome back, $USERNAME! You have played $NUMBER_OF_GAMES games, and your best game took $BEST_GAME_GUESSES guesses."
+fi
 
 NUMBER=$(( RANDOM % 1001 ))
 echo -e "the number is $NUMBER"
