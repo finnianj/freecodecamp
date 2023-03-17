c1544b01934a052a9effa6215acf8f416b7ccdfb
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-17 15:58:43 +0100

    reworked loop

--- databases/number_guess/number_guess.sh
+++ databases/number_guess/number_guess.sh
@@ -27,6 +27,26 @@ echo -e "the number is $NUMBER"
 echo -e "\nGuess the secret number between 1 and 1000:"
 GUESSES=0
 
+until [[ $NUMBER -eq $GUESS ]]
+do
+GUESSES=$(($GUESSES+1))
+read GUESS
+if [[ $GUESS  =~ ^[0-9]+$ ]]
+then
 
+  if [[ $NUMBER -lt $GUESS ]]
+  then
+    echo -e "\nIt's lower than that, guess again:"
+  elif [[ $NUMBER -gt $GUESS ]]
+  then
+    echo -e "\nIt's higher than that, guess again:"
+  else
+    echo -e "\nYou guessed it in $GUESSES tries. The secret number was $NUMBER. Nice job!"
+  fi
+
+else
+  echo -e "\nThat is not an integer, guess again:"
+fi
+done
 
 INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(user_id, guesses) VALUES($USER_ID, $GUESSES)");
