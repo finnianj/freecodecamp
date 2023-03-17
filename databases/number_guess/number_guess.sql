11be36608e58844bbf0f6bbb936ec65479235dc7
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-17 16:00:11 +0100

    sql file

--- databases/number_guess/number_guess.sql
+++ databases/number_guess/number_guess.sql
@@ -84,7 +84,7 @@ ALTER SEQUENCE public.games_game_id_seq OWNED BY public.games.game_id;
 
 CREATE TABLE public.users (
     user_id integer NOT NULL,
-    name character varying(20) NOT NULL
+    name character varying(22)
 );
 
 
@@ -130,26 +130,66 @@ ALTER TABLE ONLY public.users ALTER COLUMN user_id SET DEFAULT nextval('public.u
 -- Data for Name: games; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.games VALUES (30, 24, 317);
+INSERT INTO public.games VALUES (31, 24, 640);
+INSERT INTO public.games VALUES (32, 25, 113);
+INSERT INTO public.games VALUES (33, 25, 647);
+INSERT INTO public.games VALUES (34, 24, 680);
+INSERT INTO public.games VALUES (35, 24, 906);
+INSERT INTO public.games VALUES (36, 24, 656);
+INSERT INTO public.games VALUES (37, 26, 1);
+INSERT INTO public.games VALUES (38, 26, 2);
+INSERT INTO public.games VALUES (39, 26, 2);
+INSERT INTO public.games VALUES (40, 27, 950);
+INSERT INTO public.games VALUES (41, 27, 240);
+INSERT INTO public.games VALUES (42, 28, 667);
+INSERT INTO public.games VALUES (43, 28, 192);
+INSERT INTO public.games VALUES (44, 27, 194);
+INSERT INTO public.games VALUES (45, 27, 181);
+INSERT INTO public.games VALUES (46, 27, 766);
+INSERT INTO public.games VALUES (47, 29, 255);
+INSERT INTO public.games VALUES (48, 29, 5);
+INSERT INTO public.games VALUES (49, 30, 373);
+INSERT INTO public.games VALUES (50, 30, 498);
+INSERT INTO public.games VALUES (51, 29, 342);
+INSERT INTO public.games VALUES (52, 29, 784);
+INSERT INTO public.games VALUES (53, 29, 439);
+INSERT INTO public.games VALUES (54, 31, 191);
+INSERT INTO public.games VALUES (55, 31, 786);
+INSERT INTO public.games VALUES (56, 32, 575);
+INSERT INTO public.games VALUES (57, 32, 932);
+INSERT INTO public.games VALUES (58, 31, 701);
+INSERT INTO public.games VALUES (59, 31, 660);
+INSERT INTO public.games VALUES (60, 31, 285);
 
 
 --
 -- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
+INSERT INTO public.users VALUES (24, 'user_1679064677451');
+INSERT INTO public.users VALUES (25, 'user_1679064677450');
+INSERT INTO public.users VALUES (26, 'Finn');
+INSERT INTO public.users VALUES (27, 'user_1679065002445');
+INSERT INTO public.users VALUES (28, 'user_1679065002444');
+INSERT INTO public.users VALUES (29, 'user_1679065141018');
+INSERT INTO public.users VALUES (30, 'user_1679065141017');
+INSERT INTO public.users VALUES (31, 'user_1679065168613');
+INSERT INTO public.users VALUES (32, 'user_1679065168612');
 
 
 --
 -- Name: games_game_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.games_game_id_seq', 1, false);
+SELECT pg_catalog.setval('public.games_game_id_seq', 60, true);
 
 
 --
 -- Name: users_user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.users_user_id_seq', 1, false);
+SELECT pg_catalog.setval('public.users_user_id_seq', 32, true);
 
 
 --
@@ -179,4 +219,3 @@ ALTER TABLE ONLY public.games
 --
 -- PostgreSQL database dump complete
 --
-
