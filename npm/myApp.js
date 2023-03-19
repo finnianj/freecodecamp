d2a052a089ccf7453294e510f7d3b79092eba083
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-19 15:57:13 +0100

    defining index.html path using __dirname global variable

--- npm/myApp.js
+++ npm/myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+// app.get("/", (req, res) => {
+//   res.send('Response String')
+// });
+
+let html_index_path = __dirname + '/views/index.html'
+
 app.get("/", (req, res) => {
-  res.send('Response String')
+  res.sendFile(html_index_path)
 });
