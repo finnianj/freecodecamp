55272373eb51fb57ed1ae0988be17e6c68f038d5
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-20 11:13:37 +0100

    logger

--- npm/myApp.js
+++ npm/myApp.js
@@ -6,6 +6,11 @@ let app = express();
 
 app.use("/public", express.static(__dirname + '/public'));
 
+app.use(function(req, res, next) {
+  console.log(req.method, req.path, req.ip)
+  next();
+});
+
 // app.get("/", (req, res) => {
 //   res.send('Response String')
 // });
