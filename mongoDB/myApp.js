a5ba687df94bdf05612b79bd7cc4d7c9d9ee2269
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-21 16:44:29 +0000

    chaining queries

--- mongoDB/myApp.js
+++ mongoDB/myApp.js
@@ -121,8 +121,15 @@ const removeManyPeople = (done) => {
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  var findQuery = Person.find({ favoriteFoods: foodToSearch })
+  findQuery.sort({ name: 1 })
+          .limit(2)
+          .select({ age: 0 })
+          .exec(function(err, data) {
+              if (err) return console.error(err);
+              console.log(data)
+              done(null, data);
+          })
 };
 
 /** **Well Done !!**
