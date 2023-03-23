56343aa26a2d620ab384b1b7e5c69c33e883b763
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-23 18:14:15 +0000

    yes

--- apis/projects/timestamp_api/index.js
+++ apis/projects/timestamp_api/index.js
@@ -34,3 +34,8 @@ const convertDate = (date) => {
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
