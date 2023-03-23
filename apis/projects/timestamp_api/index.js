2acb86c5768d6dfe4cdc65cdef34bf9758e97262
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-23 18:13:28 +0000

    yes

--- apis/projects/timestamp_api/index.js
+++ apis/projects/timestamp_api/index.js
@@ -9,3 +9,6 @@ var app = express();
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
