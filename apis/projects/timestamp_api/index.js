e2539c3929377cb880ceeea032ebc555a4431f54
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-23 18:13:20 +0000

    yes

--- apis/projects/timestamp_api/index.js
+++ apis/projects/timestamp_api/index.js
@@ -4,4 +4,8 @@
 var express = require('express');
 var app = express();
 
-// ajhbflsdgf
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
