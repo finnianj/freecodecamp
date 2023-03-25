c77c7e67a358ced5e85c73997a5dba5eb0af6b9e
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-25 16:42:42 +0100

    delete many

--- apis/projects/url_shortener/index.js
+++ apis/projects/url_shortener/index.js
@@ -31,6 +31,12 @@ let Link = mongoose.model('Link', linkSchema);
 let i = 1;
 // ----------------------------
 
+Link.deleteMany({}).then((res) => {
+  //if succeded do this block of code
+  console.log(res)
+}).catch((err) => {
+  console.log("Error: " + err)
+});;
 
 
 app.get('/', function(req, res) {
