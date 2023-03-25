78b385c8adc8c4e212856cee21099460fc5f8395
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-25 18:07:35 +0100

    finaaaaalllly

--- apis/projects/url_shortener/index.js
+++ apis/projects/url_shortener/index.js
@@ -1,22 +1,17 @@
 require('dotenv').config();
-let mongoose = require("mongoose")
-const mongo_key = process.env.MONGO_URI
 const express = require('express');
+let mongoose = require("mongoose")
+const mongo_key = process.env['MONGO_KEY']
+
 const cors = require('cors');
 const app = express();
 let bodyParser = require('body-parser')
-const port = process.env.PORT || 3000;
-
+const dns = require('dns');
 
 
 // Basic Configuration
-
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
-//parsing middleware
-
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
 
 mongoose.connect(mongo_key, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Mongodb connected"))
 .catch(err => console.log(err));
@@ -24,31 +19,54 @@ mongoose.connect(mongo_key, { useNewUrlParser: true, useUnifiedTopology: true}).
 const linkSchema = new mongoose.Schema({
   url: String,
   short_url: Number
+
 });
 
 let Link = mongoose.model('Link', linkSchema);
 
-let i = 1;
-// ----------------------------
+let i = 10;
+
+// async function check(url) {
+//   let hostname = url.split('.')
+//   hostname.shift()
+//   // lookup the hostname passed as argument
+//   let response = dns.lookup(hostname.join('.'), (error, address, family) => {
+//     // if an error occurs, eg. the hostname is incorrect!
+//     if (error) {
+//       return false
+//       console.error(error);
+//     } else {
+//       // if no error exists
+//       console.log(
+//         `The ip address is ${address} and the ip version is       ${family}`)
+//       return true
+//     };
+//   });
+//   return response;
+// }
+
+app.use(cors());
+
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 Link.deleteMany({}).then((res) => {
-  //if succeded do this block of code
-  console.log(res)
-}).catch((err) => {
-  console.log("Error: " + err)
-});;
-
+    //if succeded do this block of code
+    console.log(res)
+  }).catch((err) => {
+    console.log("Error: " + err)
+});
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/:id', function(req, res) {
+app.get('/api/shorturl/:id', function(req, res) {
   Link.find({ short_url: parseInt(req.params.id, 10) }).then((ok) => {
     console.log(ok)
     res.redirect(ok[0].url);
@@ -57,24 +75,34 @@ app.get('/api/:id', function(req, res) {
   });
 });
 
-app.post("/api/shorturl", function(req, res) {
-  // console.log(checkURL(req.body.url))
-let a = new Link({
-  url: req.body.url,
-  short_url: i
-});
+app.post("/api/shorturl", async function(req, res, done) {
+let regex = new RegExp(/[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi);
 
-a.save().then((res) => {
-    //if succeded do this block of code
-    i += 1;
-    console.log("i is now equal to " + i)
-    // done(null, data)
-  }).catch((err) => {
-    console.log("Error: " + err)
-  });
+  if (req.body.url.match(regex)) {
+    console.log("valid")
+    let a = new Link({
+      url: req.body.url,
+      short_url: i
+    });
 
-res.json({ "original_url": req.body.url })
+    a.save().then((res) => {
+          //if succeded do this block of code
+          console.log("Created: " + res)
+          i += 10;
+          console.log("short url id has been incremented. It is now equal to " + i)
+          done(null, res)
+      }).catch((err) => {
+        console.log("Error: " + err)
+      });
+
+    res.json({ original_url: req.body.url, short_url: a.short_url })
+  } else {
+    console.log("invalid")
+    res.json({ error: 'invalid url'})
+  }
+
 })
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
