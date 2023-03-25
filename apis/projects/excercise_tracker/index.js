964409b1f4765d6546a31d8679491cc7c7dfaf45
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-25 23:38:43 +0100

    date

--- apis/projects/excercise_tracker/index.js
+++ apis/projects/excercise_tracker/index.js
@@ -84,6 +84,7 @@ app.get('/api/users', function(req, res) {
 
 app.post('/api/users/:_id/exercises', function(req, res) {
   // check if date field is emoty
+  // if empty
 
   User.findById({ _id: req.body[':_id']})
     .then(data => {
