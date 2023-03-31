fc7dd95629e3141910deb3fdcc13cb636c1b2e81
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-31 10:28:28 +0200

    exporting routing and auth logic

--- quality_assurance/advanced_node/routes.js
+++ quality_assurance/advanced_node/routes.js
@@ -1,3 +1,96 @@
+const passport = require('passport');
+
 module.exports = function (app, myDataBase) {
+
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please login',
+      showLogin: true,
+      showRegistration: true
+    });
+  });
+
+  app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/' }),
+  function(req, res) {
+    res.redirect('/profile');
+  }
+  );
+
+  app.get('/profile', ensureAuthenticated, function(req, res) {
+    res.render('profile', {
+      username: req.user.username
+    })
+  }
+  );
+
+  app.route('/logout')
+  .get((req, res) => {
+    req.logout();
+    res.redirect('/');
+  });
+
+  app.route('/register').post((req, res, next) => {
+
+    myDataBase.findOne({ username: req.body.username }, (err, user) => {
+      if (err) {
+        next(err);
+      } else if (user) {
+        res.redirect('/');
+      } else {
+        const hash = bcrypt.hashSync(req.body.password, 12);
+        myDataBase.insertOne({ username: req.body.username, password: hash }, (err, doc) => {
+          if (err) {
+            res.redirect('/')
+          } else {
+            next(null, doc.ops[0])
+          }
+        })
+      }
+    })
+
+  }, passport.authenticate('local', { failureRedirect: '/' }),
+    (req, res, next) => {
+      res.redirect('/profile')
+    }
+  );
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
+  passport.use(new LocalStrategy((username, password, done) => {
+    myDataBase.findOne({ username: username }, (err, user) => {
+      console.log(`User ${username} attempted to log in.`);
+      if (err) return done(err);
+      if (!user) return done(null, false);
+      if (!bcrypt.compareSync(password, user.password)) {
+        return done(null, false);
+      }
+      return done(null, user);
+    });
+  }));
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    }
+    res.redirect('/');
+  };
 
 }
