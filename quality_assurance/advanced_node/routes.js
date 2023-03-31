ce71e041767eb65d069bb1a303829c5d5bb1bbfa
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-31 11:34:48 +0200

    ok

--- quality_assurance/advanced_node/routes.js
+++ quality_assurance/advanced_node/routes.js
@@ -59,10 +59,13 @@ module.exports = function (app, myDataBase) {
     }
   );
 
-  app.route('/auth/github').get(passport.authenticate('github'))
-  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }, (req, res) => {
-    res.redirect('/profile')
-  }))
+
+
+  app.route('/auth/github').get(passport.authenticate('github'));
+  app.route('/auth/github/callback').get(passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile');
+  })
+
 
   app.use((req, res, next) => {
     res.status(404)
