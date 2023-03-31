51a711fb1f540bb0bd414f88e75ab6b1984b4634
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-31 12:00:08 +0200

    github strategy

--- quality_assurance/advanced_node/auth.js
+++ quality_assurance/advanced_node/auth.js
@@ -19,6 +19,10 @@ module.exports = function (app, myDataBase) {
   }));
 
   passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: /*INSERT CALLBACK URL ENTERED INTO GITHUB HERE*/
+  },
     
   ));
 
