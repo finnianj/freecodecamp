fefa8c74779704541ad9e2f7e3f8ebc86461d28a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-03-31 09:43:19 +0200

    404

--- quality_assurance/advanced_node/server.js
+++ quality_assurance/advanced_node/server.js
@@ -59,7 +59,13 @@ myDB(async client => {
   .get((req, res) => {
     req.logout();
     res.redirect('/');
-});
+  });
+
+  app.use((req, res, next) => {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
 
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({ username: username }, (err, user) => {
