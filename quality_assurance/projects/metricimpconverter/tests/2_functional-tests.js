a1e70330f02145879c733491e663b688141dacb6
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-03 14:56:43 +0200

    chai testing with chai http server

--- quality_assurance/projects/metricimpconverter/tests/2_functional-tests.js
+++ quality_assurance/projects/metricimpconverter/tests/2_functional-tests.js
@@ -5,12 +5,22 @@ const server = require('../server');
 
 chai.use(chaiHttp);
 
+
+
 suite('Functional Tests', function() {
 
+  test('#Convert a valid input such as 10L: GET request to /api/convert.', function(done) {
+    chai
+        .request(server)
+        .get('/api/convert/?input=10L')
+        .end(function (err, res) {
+          assert.equal(res.status, 200);
+          assert.equal(res.body.returnNum, 2.64172);
+          done();
+        });
+  })
 
 
-
-// Convert a valid input such as 10L: GET request to /api/convert.
 // Convert an invalid input such as 32g: GET request to /api/convert.
 // Convert an invalid number such as 3/7.2/4kg: GET request to /api/convert.
 // Convert an invalid number AND unit such as 3/7.2/4kilomegagram: GET request to /api/convert.
