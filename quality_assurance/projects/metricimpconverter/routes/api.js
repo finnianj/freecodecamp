7b474f95aa2ed5493ce494582fbcb9b373a75ece
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-03 09:42:49 +0200

    string splitting

--- quality_assurance/projects/metricimpconverter/routes/api.js
+++ quality_assurance/projects/metricimpconverter/routes/api.js
@@ -2,14 +2,15 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
-// const input_field = document.getElementById('convert');
 
 module.exports = function (app) {
 
   let convertHandler = new ConvertHandler();
 
   app.get('/api/convert', (req, res) => {
-    console.log(req.query.input)
+    let input = req.query.input
+    let initNum = convertHandler.getNum(input)
+    console.log(initNum)
   })
 
 };
