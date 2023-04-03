01feeb34c98fbdf0788600977c9d7c3c429064aa
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-03 09:12:05 +0200

    extracting user query

--- quality_assurance/projects/metricimpconverter/routes/api.js
+++ quality_assurance/projects/metricimpconverter/routes/api.js
@@ -2,9 +2,14 @@
 
 const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
+// const input_field = document.getElementById('convert');
 
 module.exports = function (app) {
-  
+
   let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    console.log(req.query.input)
+  })
 
 };
