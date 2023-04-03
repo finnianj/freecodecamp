b211b174d7181ecdb55a9ca48ca35bb163f30136
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-03 10:23:38 +0200

    catching errors

--- quality_assurance/projects/metricimpconverter/controllers/convertHandler.js
+++ quality_assurance/projects/metricimpconverter/controllers/convertHandler.js
@@ -1,10 +1,15 @@
 function ConvertHandler() {
 
   this.getNum = function(input) {
-    let index = input.match(/[klgm]/i).index;
-    input = input.split('')
-    let result = [ input.slice(0, index).join(''), input.slice(index).join('').toLowerCase()];
-    return result;
+    if (input.match(/[klgm]/i)) {
+      let index = input.match(/[klgm]/i).index;
+      input = input.split('')
+      let result = [ input.slice(0, index).join(''), input.slice(index).join('').toLowerCase()];
+      return result;
+    } else {
+      console.log("Invalid metric")
+      return "Invalid metric"
+    }
   };
 
   // this.getUnit = function(input) {
@@ -33,31 +38,26 @@ function ConvertHandler() {
 
     switch (initUnit) {
       case 'kg':
-        console.log("kilograms>>>")
         result = [initNum / lbsToKg, "lbs"]
         break;
       case 'gal':
-        console.log("gallons")
         result = [initNum * galToL, "L"]
         break;
       case 'l':
-        console.log("litres")
         result = [initNum / galToL, "gal"]
         break;
       case 'mi':
-        console.log("miles")
         result = [initNum * miToKm, "km"]
         break;
       case 'km':
-        console.log("kilometeres")
         result = [initNum / miToKm, "mi"]
         break;
       case 'lbs':
-        console.log("pounds")
         result = [initNum * lbsToKg, "kg"]
         break;
       default:
-        console.log("Invalid input given");
+        console.log("Invalid metric")
+        result = "Invalid metric"
     }
     return result;
   };
--- quality_assurance/projects/metricimpconverter/routes/api.js
+++ quality_assurance/projects/metricimpconverter/routes/api.js
@@ -9,13 +9,15 @@ module.exports = function (app) {
 
   app.get('/api/convert', (req, res) => {
     let input = convertHandler.getNum(req.query.input)
-    let converted = convertHandler.convert(input[0], input[1])
-    console.log(converted)
+    if (input == "Invalid metric") {
+      console.log("Invalid metric found")
+    } else {
+      let converted = convertHandler.convert(input[0], input[1])
+      let rounded = Math.round(converted[0] * 100_000) / 100_000
 
-    let string = convertHandler.getString(input[0], input[1], converted[0], converted[1])
-    console.log(string)
-    let rounded = Math.round(converted[0] * 100_000) / 100_000
-    console.log("Rounded value: " + rounded)
+      let string = convertHandler.getString(rounded, input[1], converted[0], converted[1])
+      console.log(string)
+    }
   })
 
 };
