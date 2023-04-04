98aec9fe25a3e2cb23a08c7ee54a2ab833d0a498
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-04 14:17:55 +0200

    mongoose config

--- quality_assurance/projects/issue_tracker/routes/api.js
+++ quality_assurance/projects/issue_tracker/routes/api.js
@@ -8,13 +8,51 @@ mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology:
 .catch(err => console.log(err));
 
 const issueSchema = new mongoose.Schema({
-  name: {
+  issue_title: {
     type: String,
     required: true
   },
-  age: Number,
-  favoriteFoods: [String]
+  issue_text: {
+    String,
+    required: true
+  },
+  created_by: {
+    String,
+    required: true
+  },
+  assigned_to: {
+    String,
+    default: ''
+  },
+  status_text:{
+    String,
+    default: ''
+  },
+  created_on: {
+    Date,
+    default: Date.now
+  },
+  updated_on: {
+    Date,
+    default: Date.now
+  },
+  open: {
+    Boolean,
+    default: true
+  }
 });
+
+let Issue = mongoose.model('Issue', issueSchema);
+
+const createAndSaveIssue = (issue, done) => {
+  let i = new Person(issue);
+
+  i.save(function(err, data) {
+    if (err) return console.error(err);
+    console.log(data);
+    done(null, data)
+  });
+};
 
 
 module.exports = function (app) {
@@ -28,7 +66,15 @@ module.exports = function (app) {
 
     .post(function (req, res){
       let project = req.params.project;
-
+      console.log(req.body)
+      let issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to,
+        status_text: req.body.status_text
+      }
+      createAndSaveIssue(issue)
     })
 
     .put(function (req, res){
