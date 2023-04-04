59d3ff1b0715e6e79a9fb8782170c7b3c0a45e2a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-04 17:57:50 +0200

    almost there

--- quality_assurance/projects/issue_tracker/routes/api.js
+++ quality_assurance/projects/issue_tracker/routes/api.js
@@ -160,7 +160,7 @@ module.exports = function (app) {
         return
       })
       console.log("nothing performed")
-      res.json({ error: 'could not delete', '_id': id })
+      return res.json({ error: 'could not delete', '_id': id })
       // res.json({ error: 'could not delete', '_id': id })
 
     });
