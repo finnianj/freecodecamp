eb039d585148c6c5729b51b6d10194ffa56f59c2
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-07 22:37:07 +0200

    updatinb book comments

--- quality_assurance/projects/bookstore/routes/api.js
+++ quality_assurance/projects/bookstore/routes/api.js
@@ -40,12 +40,12 @@ module.exports = function (app) {
       Book.find({})
         .then((data) => {
           let formatted_data = data.map((b) => b = { _id: b.id, title: b.title, commentcount: b.comments.length })
-          console.log("\nAll books:")
+          console.log("\nAll books:\n")
           console.log(formatted_data)
           res.json(formatted_data)
         })
         .catch((err) => {
-          console.log("Request for all books failed.")
+          console.log("Request for all books failed.\n")
           console.log(err)
         })
       //response will be array of book objects
@@ -63,8 +63,9 @@ module.exports = function (app) {
           res.json(data)
         })
         .catch((err) => {
-          console.log("Request to create book failed.")
+          console.log("Request to create book failed.\n")
           console.log(err)
+          res.send("missing required field title\n")
         })
       //response will contain new book object including atleast _id and title
     })
@@ -78,12 +79,28 @@ module.exports = function (app) {
   app.route('/api/books/:id')
     .get(function (req, res){
       let bookid = req.params.id;
+      Book.find({ _id: bookid })
+        .then((data) => {
+          console.log("\nThank you for searching for a book. Here are the results:\n")
+          console.log(data)
+          res.json(data)
+        })
+        .catch((err) => {
+          console.log("\nRequest for individual book.\n")
+          console.log(err)
+          res.send("no book exists.")
+        })
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
     })
 
     .post(function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
+      if ( comment == null || comment || undefined ) {
+        res.send("missing required field comment")
+      } else if ( bookid == null || bookid || undefined ) {
+        res.send("missing id")
+      }
       //json res format same as .get
     })
 
