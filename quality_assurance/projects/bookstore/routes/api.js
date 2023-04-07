17803779a225e77def11f7c890b0749908893500
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-07 22:05:57 +0200

    new project

---
+++ quality_assurance/projects/bookstore/routes/api.js
@@ -0,0 +1,79 @@
+/*
+*
+*
+*       Complete the API routing below
+*
+*
+*/
+
+'use strict';
+let mongoose = require("mongoose")
+require('dotenv').config();
+
+// --------- Mongo DB config -------------
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Mongodb connected"))
+.catch(err => console.log(err));
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  created_on: {
+    type: Date,
+    default: Date.now
+  },
+  updated_on: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+let Book = mongoose.model('Book', bookSchema);
+
+Issue.deleteMany({})
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.error(err)
+  })
+
+module.exports = function (app) {
+
+  app.route('/api/books')
+    .get(function (req, res){
+      //response will be array of book objects
+      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+    })
+
+    .post(function (req, res){
+      let title = req.body.title;
+      //response will contain new book object including atleast _id and title
+    })
+
+    .delete(function(req, res){
+      //if successful response will be 'complete delete successful'
+    });
+
+
+
+  app.route('/api/books/:id')
+    .get(function (req, res){
+      let bookid = req.params.id;
+      //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
+    })
+
+    .post(function(req, res){
+      let bookid = req.params.id;
+      let comment = req.body.comment;
+      //json res format same as .get
+    })
+
+    .delete(function(req, res){
+      let bookid = req.params.id;
+      //if successful response will be 'delete successful'
+    });
+
+};
