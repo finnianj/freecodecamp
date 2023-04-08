fe9418e379adcbc08c7685bc2aaf6ae1ec233c85
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-08 11:45:07 +0200

    feat: functional tests

--- quality_assurance/projects/bookstore/tests/2_functional-tests.js
+++ quality_assurance/projects/bookstore/tests/2_functional-tests.js
@@ -64,19 +64,25 @@ suite('Functional Tests', function() {
           assert.equal(res.text, 'missing required field title')
           done();
         });
-        //done();
       });
 
     });
 
 
-    // suite('GET /api/books => array of books', function(){
+    suite('GET /api/books => array of books', function(){
 
-    //   test('Test GET /api/books',  function(done){
-    //     //done();
-    //   });
+      test('Test GET /api/books',  function(done){
+        chai.request(server)
+        .get('/api/books')
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.isArray(res.body)
+          assert.property(res.body[0], 'title')
+          done();
+        });
+      });
 
-    // });
+    });
 
 
     // suite('GET /api/books/[id] => book object with [id]', function(){
