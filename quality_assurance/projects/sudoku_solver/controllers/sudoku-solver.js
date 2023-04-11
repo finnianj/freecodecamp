68cf4ca3d51695c40806c3b580300f93714ddd4a
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-11 10:59:22 +0200

    feat: col checker

--- quality_assurance/projects/sudoku_solver/controllers/sudoku-solver.js
+++ quality_assurance/projects/sudoku_solver/controllers/sudoku-solver.js
@@ -10,6 +10,7 @@ class SudokuSolver {
     let row_index = (row_letter % 65);
     console.log("Row index: " + row_index)
     let row = puzzleString.split('').slice(row_index, (row_index + 10))
+    console.log("Is this row value already taken? " + row.some((item) => item == value))
     return row.some((item) => item == value)
   }
 
@@ -17,7 +18,7 @@ class SudokuSolver {
     let col_num = column - 1
     let array = puzzleString.split('')
     let col_extract = array.filter((_num, i) => i % 9 == col_num)
-    console.log("Is this value already taken? " + col_extract.some(num => num == value))
+    console.log("Is this column value already taken? " + col_extract.some(num => num == value))
     return col_extract.some(num => num == value)
   }
 
