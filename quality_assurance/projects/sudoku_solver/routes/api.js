4968811ef991666c67bf5f1d3545e0c57d3b9184
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-12 12:35:50 +0200

    feat: solve function

--- quality_assurance/projects/sudoku_solver/routes/api.js
+++ quality_assurance/projects/sudoku_solver/routes/api.js
@@ -2,15 +2,7 @@
 
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 'use strict';
-let mongoose = require("mongoose")
 require('dotenv').config();
-
-// --------- Mongo DB config -------------
-
-mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log("Mongodb connected"))
-.catch(err => console.log(err));
-
-// ---------------------------------------
 
 module.exports = function (app) {
 
