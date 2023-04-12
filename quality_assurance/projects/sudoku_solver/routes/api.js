9028ca89e782e801ef18786eb0eb4dd83ba63ba5
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-12 16:06:57 +0200

    feat: unit tests

--- quality_assurance/projects/sudoku_solver/routes/api.js
+++ quality_assurance/projects/sudoku_solver/routes/api.js
@@ -51,7 +51,6 @@ module.exports = function (app) {
 
   app.route('/api/solve')
     .post((req, res) => {
-      console.log(req.body)
       if ( !req.body.puzzle ) {
         console.log('Required field(s) missing')
         return res.json({ error: 'Required field missing' })
--- quality_assurance/projects/sudoku_solver/tests/1_unit-tests.js
+++ quality_assurance/projects/sudoku_solver/tests/1_unit-tests.js
@@ -31,6 +31,50 @@ suite('Unit Tests', () => {
     });
   });
 
+  test('Logic handles a puzzle string that is not 81 characters in length', function(done) {
+    chai.request(server)
+    .post('/api/solve')
+    .send({
+      puzzle: "1.5..2.84...3.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.."
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.equal(res.body.error, 'Expected puzzle to be 81 characters long')
+      done();
+    });
+  });
+
+  test('Logic handles a valid row placement', function(done) {
+    chai.request(server)
+    .post('/api/check')
+    .send({
+      puzzle: "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+      coordinate: "A1",
+      value: 7
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.equal(res.body.valid, true )
+      done();
+    });
+  });
+
+  test('Logic handles an invalid row placement', function(done) {
+    chai.request(server)
+    .post('/api/check')
+    .send({
+      puzzle: "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+      coordinate: "A2",
+      value: 4
+    })
+    .end(function(err, res){
+      assert.equal(res.status, 200);
+      assert.equal(res.body.valid, false )
+      assert.equal(res.body.conflict, "row" )
+      done();
+    });
+  });
+
 
 
 });
