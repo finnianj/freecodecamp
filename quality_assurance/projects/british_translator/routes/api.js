63c56aa5ea597f40ccbe457b934285b1d612ca5d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-13 13:52:08 +0200

    guard

--- quality_assurance/projects/british_translator/routes/api.js
+++ quality_assurance/projects/british_translator/routes/api.js
@@ -3,11 +3,13 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      if (! req.body.locale || !req.body.text) return res.json({ error: 'Required field(s) missing' })
+      if ( req.body.text == '' ) return res.json({ error: 'No text to translate' })
+      if (req.body.locale != 'american-to-british' || req.body.locale != 'british-to-american' ) return res.json({ error: 'Invalid value for locale field' })
     });
 };
