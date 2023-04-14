0e9e478f2ff64172517b9dc3edf406d2316a0430
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2023-04-14 11:44:34 +0200

    file management

--- quality_assurance/projects/sudoku_solver/tests/1_unit-tests.js
+++ quality_assurance/projects/sudoku_solver/tests/1_unit-tests.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const assert = chai.assert;
 const server = require('../server');
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
 
 suite('Unit Tests', () => {
   test('Logic handles a valid puzzle string of 81 characters', function(done) {
